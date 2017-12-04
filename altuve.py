5b73120134480fb0dbfc00a6f15d5aac0eb2d757
Dave G Vigil SR <dgvigi@sandia.gov>
2017-12-04 16:33:52 -0700

    Playing with different plotting methods in Python

--- altuve.py
+++ altuve.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 all_altuve = pd.read_csv("Altuve-2016-hitting.csv")
 
-all_altuve.head()
+#all_altuve.head()
 
-all_altuve.plot.scatter(x='x',y='y').invert_yaxis;
+hit_types = all_altuve.groupby('des')
+
+fig, ax = plt.subplots()
+ax.margins(0.05)
+for name, group in hit_types:
+    ax.plot(group.x, group.y, marker='o', linestyle='', ms=3, label=name)
+ax.legend()
+
 plt.show()
 
+y_max = max(all_altuve['y']) + 25
+plt.ylim(y_max, -5)
+
+plt.show()
