ba0259e54918a7a6a260478c596541aed5256d80
David G Vigil Sr <dgvigil@gmail.com>
2018-01-17 21:17:16 -0700

    Added Ben's ideas

---
+++ ben01.py
@@ -0,0 +1,2 @@
+# which outfeild position has the most put outs. Is it becuase they get more
+# chances or the better players make the plays
---
+++ ben02.py
@@ -0,0 +1,4 @@
+# Which is the best pitch and count to hit when there is a runner on 2nd base
+#
+# plan of action: Find all at bats when man on second. find last pitch on that
+# at bat. figure out the possible results and see what story it tells.
